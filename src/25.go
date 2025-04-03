acf29929ee192d6707f6764fda432d2c991f5a3e
sharnettaarc <sharnettaarc@gmail.com>
2025-04-03 23:16:46 +0000

    Add 25.go

---
+++ src/25.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int = 5
+    if x > 10 {
+        fmt.Println("x is greater than 10")
+    } else {
+        fmt.Println("x is less than or equal to 10")
+    }
+}
