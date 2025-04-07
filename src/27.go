90bf048cf2f54471cb7c7f36265d2a88aef9c7a9
sharnettaarc <sharnettaarc@gmail.com>
2025-04-07 03:46:21 +0000

    Add 27.go

---
+++ src/27.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    num := 5
+    if num < 10 {
+        fmt.Println("The number is less than 10.")
+    } else {
+        fmt.Println("The number is not less than 10.")
+    }
+}
